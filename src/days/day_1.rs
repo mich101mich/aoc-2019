a9e0503d6d3a2feae91617389b3f9b4a86dadb24
mich101mich <mich101mich@gmail.com>
2019-11-25 13:00:33 +0100

    update

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,11 +1,9 @@
-pub use crate::utils::*;
-use rayon::prelude::*;
+use crate::utils::*;
 
 pub fn run() {
 	#[allow(unused_variables)]
 	let input = include_str!("../input/1.txt");
 	let input = "";
 	
-	println!("input: {:?}", input);
-	pv!(input);
+	
 }

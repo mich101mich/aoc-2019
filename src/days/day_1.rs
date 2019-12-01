9c8091cea0c94199080332648fc4cd41ff7517a3
mich101mich <mich101mich@gmail.com>
2019-12-01 07:40:37 +0100

    added day 01

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -3,7 +3,30 @@ use crate::utils::*;
 pub fn run() {
 	#[allow(unused_variables)]
 	let input = include_str!("../input/1.txt");
-	let input = "";
-	
-	
+	// let input = "";
+	let parsed = input
+		.lines()
+		.map(|line| isize::from_str(line).unwrap())
+		.map(|n| n / 3 - 2);
+	let mut total = 0;
+	for mut n in parsed {
+		while n > 0 {
+			total += n;
+			n = n / 3 - 2;
+		}
+	}
+	println!("parsed: {:?}", total);
+}
+
+#[allow(unused)]
+pub fn part_one() {
+	#[allow(unused_variables)]
+	let input = include_str!("../input/1.txt");
+	// let input = "";
+	let parsed: usize = input
+		.lines()
+		.map(|line| usize::from_str(line).unwrap())
+		.map(|n| n / 3 - 2)
+		.sum();
+	println!("parsed: {:?}", parsed);
 }

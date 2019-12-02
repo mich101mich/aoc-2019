0c28e1dfedbcb404d0c34c3b9692cddd7a26b3b3
mich101mich <mich101mich@gmail.com>
2019-12-02 05:57:18 +0100

    updated script

--- setup.sh
+++ setup.sh
@@ -3,13 +3,24 @@ echo "use crate::utils::*;
 
 pub fn run() {
 	#[allow(unused_variables)]
-	let input = include_str!("../input/$DAY.txt");
-	let input = "";
+	let input = include_str!(\"../input/$DAY.txt\");
+	// let input = "";
 	
+	let parsed = input
+		.lines()
+		//.map(|l| i32::from_str(l).unwrap_or_else(|_| panic!(\"failed to parse >{}<\", l)))
+		//.map(|l| scanf!(l, \"{}\", i32))
+		//.to_vec();
+		//.sum::<i32>();
+	
+	//pv!(parsed);
 	
 }" > src/days/day_$DAY.rs
 
 echo "#![allow(unused_imports)]
+
+#[macro_use]
+extern crate scan_fmt;
 
 #[macro_use]
 mod utils;
@@ -25,4 +36,4 @@ pub mod day_$DAY;" >> src/days/mod.rs
 
 touch src/input/$DAY.txt
 
-code src/days/day_$DAY.rs src/input/1.txt 
+code src/days/day_$DAY.rs src/input/$DAY.txt 

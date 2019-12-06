b9876668a70a1e223e341f5c881de7ad8fe912fa
mich101mich <mich101mich@gmail.com>
2019-12-06 06:20:57 +0100

    added day 6

---
+++ src/days/day_6.rs
@@ -0,0 +1,77 @@
+use crate::utils::*;
+
+pub fn run() {
+	#[allow(unused_variables)]
+	let input = include_str!("../input/6.txt");
+	// let input = "";
+	let parsed = input
+	.lines()
+	.map(|l| l.split(')').to_vec())
+	//.map(|l| i32::from_str(l).unwrap_or_else(|_| panic!("failed to parse >{}<", l)))
+	//.map(|l| scanf!(l, "{}", i32))
+		.to_vec()
+		//.sum::<i32>()
+		;
+
+	let mut parent = HashMap::new();
+	for x in parsed {
+		parent.entry(x[0]).or_insert_with(|| vec![]).push(x[1]);
+	}
+	pv!(parent);
+
+	let path = dijkstra_search(
+		|p| {
+			let mut iter: Box<dyn Iterator<Item = &str>> = Box::new(
+				parent
+					.iter()
+					.filter(move |(_, v)| v.contains(&p))
+					.map(|(v, _)| *v),
+			);
+			if let Some(v) = parent.get(p) {
+				iter = Box::new(iter.chain(v.iter().copied()));
+			}
+			iter
+		},
+		|_, _| 1,
+		|_| true,
+		"YOU",
+		&["SAN"],
+	);
+	pv!(path);
+}
+
+#[allow(unused)]
+fn c(parent: &HashMap<&str, Vec<&str>>, current: &str) -> usize {
+	let child = match parent.get(current) {
+		Some(b) => b,
+		None => return 0,
+	};
+	let mut cn = child.len();
+	for chil in child.iter() {
+		cn += c(parent, chil);
+	}
+	cn
+}
+
+#[allow(unused)]
+pub fn part_one() {
+	#[allow(unused_variables)]
+	let input = include_str!("../input/6.txt");
+	// let input = ;
+	let parsed = input
+	.lines()
+	.map(|l| l.split(')').to_vec())
+	//.map(|l| i32::from_str(l).unwrap_or_else(|_| panic!("failed to parse >{}<", l)))
+	//.map(|l| scanf!(l, "{}", i32))
+	.to_vec()
+	//.sum::<i32>()
+	;
+
+	let mut count = 0;
+	let mut parent = HashMap::new();
+	for x in parsed {
+		parent.entry(x[0]).or_insert_with(|| vec![]).push(x[1]);
+	}
+
+	pv!(count);
+}
